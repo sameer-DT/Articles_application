2eb5b80dcbefe9077c396b048c51736b6f66771e
DT_sameer <sameer.shaik@dispatchtrack.com>
2024-07-18 13:08:20 +0530

    User Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  validates :username , presence: true , uniqueness:{case_sensitive:false},length: {minimum:3, maximum:30}
+
+  validates :email , presence: true , 
+            uniqueness:{case_sensitive:false},
+            length: {maximum:105},
+            format: {with: URI::MailTo::EMAIL_REGEXP}
+
 end
